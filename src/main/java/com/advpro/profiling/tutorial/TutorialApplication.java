8f1b81dcb291788df1dc691deebb4ac86270e9b0
mugmold <mugmold@gmail.com>
2026-04-27 21:27:10 +0700

    fix: add .env file support & edit dataseed parameter

--- src/main/java/com/advpro/profiling/tutorial/TutorialApplication.java
+++ src/main/java/com/advpro/profiling/tutorial/TutorialApplication.java
@@ -1,13 +1,15 @@
 package com.advpro.profiling.tutorial;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TutorialApplication {
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
-	public static void main(String[] args) {
-		SpringApplication.run(TutorialApplication.class, args);
-	}
-
+        SpringApplication.run(TutorialApplication.class, args);
+    }
 }
